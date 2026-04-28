605cf169703fc9bb08196b3fea2f021241057140
beachesow <anastasiatikhony@gmail.com>
2026-04-28 19:35:21 +0300

    Remove build artifacts and database files

--- build/Desktop_Qt_6_10_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_Planes_FILE C:/Users/ppopi/Desktop/pl/build/Desktop_Qt_6_10_2_MinGW_64_bit-Debug/Planes.exe)
-set(__QT_DEPLOY_TARGET_Planes_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_Planes_RUNTIME_DLLS C:/Qt/6.10.2/mingw_64/bin/Qt6Sql.dll;C:/Qt/6.10.2/mingw_64/bin/Qt6Charts.dll;C:/Qt/6.10.2/mingw_64/bin/Qt6OpenGLWidgets.dll;C:/Qt/6.10.2/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.10.2/mingw_64/bin/Qt6OpenGL.dll;C:/Qt/6.10.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.10.2/mingw_64/bin/Qt6Core.dll)
